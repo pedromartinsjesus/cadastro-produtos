a89aa160eb310dbec4c3fd2db4f477dd5e8d4b97
Pedro <pedromjesusti@gmail.com>
2021-06-07 23:47:47 -0300

    Instalacao e configuracao do Bootwatch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="alert alert-dismissible alert-warning">
+      <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
+      <h4 className="alert-heading">Warning!</h4>
+      <p className="mb-0">Hello , <a href="#" className="alert-link">vel scelerisque nisl consectetur et</a>.</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootswatch/dist/cerulean/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
 
